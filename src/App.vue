0e737b490e7046a82c2c48b113e10bf7a4f54888
Alex <sasha.stitkov@rambler.ru>
2021-03-12 13:28:42 +0300

    add html

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <div class="header">
-      <img src="./assets/image/logo-top.svg" alt="logo-top.svg">
+      <a href=""><img src="./assets/image/logo-top.svg" alt="logo-top.svg"></a>
       <ul class="menu">
-        <li class="menu__item"><a href="" class="menu__link">Каталог</a></li>
-        <li class="menu__item"><a href="" class="menu__link">Доставка</a></li>
+        <li v-for="item in menu" :key="item.name" class="menu__item"><a :href="item.link" class="menu__link">{{item.name}}</a></li>
       </ul>
       <form action="" class="search">
         <input type="text" class="search__input">
@@ -13,21 +12,23 @@
     </div>
     <div class="main">
       <h1>Картины эпохи Возрождения</h1>
-      <div class="card">
-        <img src="./assets/image/1.jpg" alt="1.jpg" class="card__img">
-        <div class="card__head">«Рождение Венеры» Сандро Боттичелли</div>
-        <div class="card__bottom">
-          <div class="card__sale">2 000 000 $</div>
-          <div class="card__price">1 000 000 $</div>
+      <div class="card" v-for="item in catalog" :key="item.name">
+        <img :src="item.img" :alt="item.img" class="card__img">
+        <div class="card__head">{{item.name}}</div>
+        <div v-if="true" class="card__bottom">
+          <div v-if="item.sale" class="card__sale">{{item.sale}}</div>
+          <div class="card__price">{{item.price}}</div>
           <button class="btn">Купить</button>
+        </div>
+        <div v-if="false" class="card__bottom">
+          Продана на аукционе
         </div>
       </div>
     </div>
     <div class="footer">
-      <img src="./assets/image/logo-bot.svg" alt="logo-bot.svg">
+      <a href=""><img src="./assets/image/logo-bot.svg" alt="logo-bot.svg"></a>
       <ul class="menu">
-        <li class="menu__item"><a href="" class="menu__link">Каталог</a></li>
-        <li class="menu__item"><a href="" class="menu__link">Доставка</a></li>
+        <li v-for="item in menu" :key="item.name" class="menu__item"><a :href="item.link" class="menu__link">{{item.name}}</a></li>
       </ul>
       <div class="footer__phone">
         <img src="./assets/image/phone.svg" alt="phone.svg">
@@ -42,11 +43,30 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data () {
     return {
+      menu: [{
+        name: 'Каталог',
+        link: ''
+      },
+      {
+        name: 'Доставка',
+        link: ''
+      },
+      {
+        name: 'Оплата',
+        link: ''
+      },
+      {
+        name: 'Контакты',
+        link: ''
+      },
+       {
+        name: 'О галерее',
+        link: ''
+      }],
       phone: '+7 (495) 555-55-55',
       adress: 'г. Москва, ул. Расплетина, 24',
       catalog:[
@@ -54,25 +74,25 @@ export default {
           name: '«Рождение Венеры» Сандро Боттичелли',
           sale: '2 000 000 $',
           price: '1 000 000 $',
-          img: '1'
+          img: require('./assets/image/1.jpg')
         },
         {
           name: '«Тайная вечеря»  Леонардо да Винчи',
           sale: '',
           price: '3 000 000 $',
-          img: '2'
+          img: require('./assets/image/2.jpg')
         },
         {
           name: '«Сотворение Адама» Микеланджело',
           sale: '6 000 000 $',
           price: '5 000 000 $',
-          img: '3'
+          img: require('./assets/image/3.jpg')
         },
         {
           name: '«Урок анатомии»  Рембрандт',
           sale: '',
           price: '4 000 000 $',
-          img: '4'
+          img: require('./assets/image/4.jpg')
         },
       ]
     }
